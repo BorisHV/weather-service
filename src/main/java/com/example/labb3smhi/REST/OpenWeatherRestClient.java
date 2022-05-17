f21e57a22f5099a4f5bea2c65d528766929f3da9
BorisHV <boris.holmgren.v@gmail.com>
2022-05-17 11:46:45 +0200

    All methods added to both MET and SMHI classes. Two new Repository classes added. BestWeatherController klassen tillagd. En del logik tillagd. HighestTemp fungerar som den ska...

---
+++ src/main/java/com/example/labb3smhi/REST/OpenWeatherRestClient.java
@@ -0,0 +1,24 @@
+package com.example.labb3smhi.REST;
+
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+public class OpenWeatherRestClient {
+
+    String uri = "https://api.openweathermap.org/data/3.0/onecall?lat=59.3110&lon=18.0300&appid=12f0b17b028dc893417b2b1b1ad9ac79";
+
+    public String getAllWeatherDataOpenWeather(){
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setAccept(List.of(MediaType.APPLICATION_JSON));
+        HttpEntity<String> httpEntity = new HttpEntity<>(httpHeaders);
+        ResponseEntity<String> weather = restTemplate.exchange(uri, HttpMethod.GET, httpEntity, String.class);
+
+        if(weather.getStatusCode() != HttpStatus.OK){
+            throw new RuntimeException("getAllWeatherDataOpenWeather failed");
+        }
+        return weather.getBody().toString();
+    }
+}
